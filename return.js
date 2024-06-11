d708269e1088d1897d78ee91218fb7d4ba6ff5e5
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-11 16:45:46 -0500

    ejercicio del return

---
+++ return.js
@@ -0,0 +1,14 @@
+function miFuncion(nombre, apellido, edad){
+    console.log("el nombre de la persona es:", nombre)
+    console.log("el nombre de la persona es:", apellido)
+    console.log("el nombre de la persona es:", edad)
+
+
+
+    return "la funcion se ejecuta correctamente"
+
+
+
+}
+
+console.log("resultado de la funcion:", miFuncion("mauro", "dimiri", 20))
