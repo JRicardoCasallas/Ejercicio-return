cd7ae6f9347041b29f61432f7c951a3d99741c44
Jose Ricardo Casallas <josericardocasasllas11@gmail.com>
2024-06-11 17:12:21 -0500

    ejercicio 2

--- return.js
+++ return.js
@@ -1,14 +1,27 @@
-function miFuncion(nombre, apellido, edad){
-    console.log("el nombre de la persona es:", nombre)
-    console.log("el nombre de la persona es:", apellido)
-    console.log("el nombre de la persona es:", edad)
+// function miFuncion(nombre, apellido, edad){
+//     console.log("el nombre de la persona es:", nombre)
+//     console.log("el nombre de la persona es:", apellido)
+//     console.log("el nombre de la persona es:", edad)
 
 
 
-    return "la funcion se ejecuta correctamente"
+//     return "la funcion se ejecuta correctamente"
 
 
 
+// }
+
+// console.log("resultado de la funcion:", miFuncion("mauro", "dimiri", 20))
+
+
+function totalDeProductos(producto1, producto2, producto3){
+    var resultado = producto1 + producto2 + producto3
+    console.log(resultado)
 }
 
-console.log("resultado de la funcion:", miFuncion("mauro", "dimiri", 20))
+function cobrar(){
+    console.log("le debe cobrar al cliente", totalDeProductos(10,15,10))
+
+}
+cobrar()
+
